c8fcd4ae01618b79c0fff1c4407b2298606479aa
Kiran9398 <117990143+Kiran9398@users.noreply.github.com>
2022-12-01 10:22:16 +0530

    Add files via upload

---
+++ src/ProductItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Grid, Card, CardContent, Rating } from "@mui/material";
+
+export const ProductItem = ({ item }) => {
+  return (
+    <Grid item xs={3}>
+      <Card sx={{ bgcolor: "#d03c1d", textAlign: "center" }}>
+        <CardContent>
+          <img src={item.image} width="200px" height="200px" />
+          <h3>{item.title.slice(0, 25) + "..."}</h3>
+          <h5>{item.category}</h5>
+          <Rating name="rate" value={item.rating.rate} />
+          {"(" + item.rating.count + ")"}
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+};
+
+export default ProductItem;
